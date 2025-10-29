7fae22f1e563a20d07c0710a048c47c6172eeba4
Ibrahim Mansoor <171342825+ibmansoor@users.noreply.github.com>
2025-10-28 19:08:11 -0600

    Add delete city implementation in CustomList

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,7 +18,7 @@ public class CustomList {
         return cities.contains(city);
     }
     
-
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -27,4 +27,5 @@ public class CustomListTest {
         list.deleteCity(city);
         assertFalse(list.hasCity(city));
     }
+
 }
