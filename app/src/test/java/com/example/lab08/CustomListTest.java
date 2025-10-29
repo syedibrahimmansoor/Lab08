bda6eab4f8a6f53bbf5565a1480f7bf2b2208cbb
Ibrahim Mansoor <171342825+ibmansoor@users.noreply.github.com>
2025-10-28 18:49:32 -0600

    Add hasCity method in class CustomList

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,14 +2,21 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+
+    private CustomList list;
+
+    private CustomList mockCityList() {
+        list = new CustomList();
+        list.addCity(new City("Charlottetown", "PEI"));
+        return list;
+    }
 
     @Test
     public void testHasCity() {
-        CustomList<String> cities = new CustomList<>();
-        cities.add("New York");
-
-        assertTrue(cities.hasCity("New York"));
-
+        list = mockCityList();
+        City city = new City("Charlottetown", "PEI");
+        assertTrue(list.hasCity(city));
     }
 }
