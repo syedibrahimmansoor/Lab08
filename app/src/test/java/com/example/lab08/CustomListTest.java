41e11d2a8ae886c12b8edf5d4b6cc6e5cc719704
Ibrahim Mansoor <171342825+ibmansoor@users.noreply.github.com>
2025-10-28 18:32:54 -0600

    Add first failing test (Step 5)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList<String> cities = new CustomList<>();
+        cities.add("New York");
+
+        assertTrue(cities.hasCity("New York"));
+
+    }
 }
